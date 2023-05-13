ae25ef19439b5dd9f3c6954c0db286858e8794e7
Anil Kumar Behera <96728156+Anonymous961@users.noreply.github.com>
2023-05-13 18:56:36 +0530

    pages added

---
+++ screens/review.tsx
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View} from 'react-native'
+
+const Review = () => {
+    return ( 
+        <View style={styles.container}>
+            <Text>Review Screen</Text>
+        </View>
+     );
+}
+ 
+export default Review;
+
+const styles=StyleSheet.create({
+    container:{
+        padding:24
+    }
+})
